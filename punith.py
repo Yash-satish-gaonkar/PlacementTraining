28c8b70ec3c1129c4827db7bbac46d5fa4e68d70
Yash-satish-gaonkar <149868396+Yash-satish-gaonkar@users.noreply.github.com>
2024-06-12 09:03:17 +0530

    Add files via upload

---
+++ punith.py
@@ -0,0 +1,68 @@
+# STRING
+# a="punith"
+# for i in a:
+#     print(i)
+# print('length=',len(a))
+
+# SLICE
+# a="ee sala cup namde"
+# print(a[:6])
+
+# a="ee sala cup namde"
+# print(a[-7:-1])
+
+# UPPER?LOWER
+# a='yash'
+# b='YASH'
+# print(a.upper())
+# print(b.lower())
+
+# STRIP
+# a="jai heee"
+# print(a.strip())
+
+# replace
+# a="namo modi"
+# print(a.replace("o","e"))
+
+# split
+# a="jai,baisab "
+# print(a.split(",")) list can into part so it is called mutable
+
+# escape funtion
+# a="My name is \"punith\" gowda"
+# print(a)
+
+# aptitude
+# logical reasoning
+# english
+
+# programming part
+
+# adding,append
+# a=["mang","mandya","bangalore"]
+# a.insert(1,"mang")
+# a.append("mangalore")
+# print(a)
+
+# extending a list
+# a=["Dosa","Roti"]
+# b=["tea","coffee"]
+# print(a.extend(b))
+# a.remove("Dosa")
+# b.pop(1)
+
+# a=["orange","mango","apple"]
+# a.sort()
+# print(a)
+# b=[1,23,5,6]
+# b.sort()
+# print(b)
+
+a=["orange","mango","apple"]
+b=a.copy()
+print(b)
+# a=["mang","mandya","bangalore"]
+# b=["orange","mango","apple"]
+# a.extend(b)
+# print(b)
